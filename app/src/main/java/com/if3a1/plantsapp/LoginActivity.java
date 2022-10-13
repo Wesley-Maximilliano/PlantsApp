f83b425224194537253b6f14c4d078b4750e442e
Wesley-Maximilliano <wesleymaxi56@mhs.mdp.ac.id>
2022-10-13 21:05:14 +0700

    Plants App Pertemuan 6

---
+++ app/src/main/java/com/if3a1/plantsapp/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.if3a1.plantsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
